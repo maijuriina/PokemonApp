eedc7d41e64729cde3bfef455a6b480634cb6af4
Maijuriina Mustonen <maiju.mustonen@gmail.com>
2021-12-10 12:16:25 +0200

    Random unique index finds pokemons and there are no more duplicates.

--- src/app/service/pokemon.service.ts
+++ src/app/service/pokemon.service.ts
@@ -4,7 +4,6 @@ import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { APIResource, BerryClient, NamedAPIResource } from 'pokenode-ts';
 import { PokemonClient } from 'pokenode-ts';
-import { Pokemon } from '../item/pokemon';
 
 @Injectable({
     providedIn: 'root' // fixes NullInjectorError
@@ -18,31 +17,31 @@ export class ConfigService {
         // The next calls within 5 seconds will come from the cache
     }
 
-    getPokemonList()/*: Array<Pokemon>*/ {
+    getPokemonList() {
         (async () => {
             const api = new PokemonClient();
             var amountOfPokemons = 898; // amount of pokemon without counting variations
             var amountWanted = 20;
-            var previousPokemons: (NamedAPIResource | APIResource)[] = [];
+            var griddedPokemons: (NamedAPIResource | APIResource)[] = [];
 
             await api
             .listPokemons(0, amountOfPokemons)
             .then((data) => {
-                for (var i = 0; i <= amountWanted; i++) {
-
-                   // hae randomilla arvolla seuraavaksi.
-                    previousPokemons.push(data.results[i]);
-
-                    //console.log(previousPokemons.values);
+                var indexArray = new Array();
+                for (var i = 1; i <= amountWanted; i++) {
+                    var randomPokemonIndex = Math.floor(Math.random() * amountOfPokemons);
+                    if (indexArray.includes(randomPokemonIndex)) { // if index is already in array, it is not added and random is made again
+                        //console.log(randomPokemonIndex)
+                        i--;
+                        continue;  
+                    } else { // if index is not in array, it is used to push data result to griddedPokemons
+                        indexArray.push(randomPokemonIndex);
+                        griddedPokemons.push(data.results[randomPokemonIndex]);
+                    }
                 }
-            }
-            )
-
-            //.then((data) => console.log(data.results))
+            })
             .catch((error) => console.error(error));
-            console.log(JSON.stringify(previousPokemons));
-            
-
+            console.log(JSON.stringify(griddedPokemons));    
         })();
     }
 
